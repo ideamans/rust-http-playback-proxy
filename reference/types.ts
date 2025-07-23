808e4a4eb4586c81895af884470dbc3ff807b6d0
Kunihiko Miyanaga <miyanaga@ideamans.com>
2025-07-23 11:29:50 +0900

    設計

---
+++ reference/types.ts
@@ -0,0 +1,48 @@
+export type HttpHeaders = { [key: string]: string };
+
+export type ContentEncodingType =
+  | "gzip"
+  | "compress"
+  | "deflate"
+  | "br"
+  | "identity";
+
+export interface Resource {
+  method: string;
+  url: string;
+  ttfbMs: number;
+  mbps?: number;
+  statusCode?: number;
+  errorMessage?: string;
+  rawHeaders?: HttpHeaders;
+  contentEncoding?: ContentEncodingType;
+  contentTypeMime?: string;
+  contentTypeCharset?: string;
+  contentFilePath?: string;
+  contentUtf8?: string;
+  contentBase64?: string;
+  minify?: boolean;
+}
+
+export type DeviceType = "desktop" | "mobile";
+
+export interface Inventory {
+  entryUrl?: string;
+  deviceType?: DeviceType;
+  resources: Resource[];
+}
+
+export interface BodyChunk {
+  chunk: Buffer;
+  targetTime: number;
+}
+
+export interface Transaction {
+  method: string;
+  url: string;
+  ttfb: number;
+  statusCode?: number;
+  errorMessage?: string;
+  rawHeaders?: HttpHeaders;
+  chunks: BodyChunk[];
+}
